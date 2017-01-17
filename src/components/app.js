8c464b1dc5141ed14c1247213b9493caf938d780
Ben Mercer <ben@henroben.co.uk>
2017-01-17 16:33:49 +0000

    S4:L49 - Conditional Rendering

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import BookList from '../containers/BookList';
+import BookDetail from '../containers/BookDetail';
 
 export default class App extends Component {
   render() {
     return (
-      <div>
-          <BookList />
+      <div className="row">
+          <BookList className="col-md-3" />
+          <BookDetail className="col-md-9" />
       </div>
     );
   }
